d5988c1318cd3c35bdea116ae43e18388b30c65d
phukon <hello@rikiphukon.com>
2025-01-14 04:49:17 +0530

    ci: setup release workflow

---
+++ release.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  branches: ["main", { name: "next", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }]
+  ]
+};
